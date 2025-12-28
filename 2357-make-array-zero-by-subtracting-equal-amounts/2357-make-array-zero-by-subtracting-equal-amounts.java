c15b8180a3074bb0df990436c8bb466ede1adea3
NANDANI22 <136251117+NANDANI22@users.noreply.github.com>
2025-12-28 09:09:36 +0530

    Time: 5 ms (14.26%), Space: 43.3 MB (9.39%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int count = 0;
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                for(int j=i; j<nums.length; j++){
+                nums[j] = nums[j] - temp;
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}
