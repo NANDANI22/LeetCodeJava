54ffc3290083e07f811881e6d573f0024cc1ba1c
NANDANI22 <136251117+NANDANI22@users.noreply.github.com>
2025-12-28 09:00:41 +0530

    Time: 0 ms (100%), Space: 43.3 MB (34.81%) - LeetHub

---
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int freq[] = new int[26];
+        for(char ch : word1.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        int freq2[] = new int[26];
+        for(char ch : word2.toCharArray()){
+            freq2[ch - 'a']++;
+        }
+        for(int i=0; i<26; i++){
+            if(Math.abs(freq[i] - freq2[i]) > 3){
+                return false;
+            }
+        }
+        return true;
+    }
+}
