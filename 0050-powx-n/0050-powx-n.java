206594873cca6e1b80691492dafd2c91e16bd9de
Nandani Masurkar <136251117+NANDANI22@users.noreply.github.com>
2026-01-31 23:38:03 +0530

    Time: 0 ms (100%), Space: 48.3 MB (30.07%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n < 0) {
+            return 1/power(x , n);
+        }
+        return power(x , n);
+    }
+    public static double power(double x , int n){
+        if(n == 0) return 1;
+        double ans = power(x , n/2);
+        if(n % 2 == 0){
+            return ans * ans;
+        }
+        return ans * ans * x;
+    }
+}
