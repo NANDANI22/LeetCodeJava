bf836e097bbb68897dc272a224c2eb59dfa43e81
Nandani Masurkar <136251117+NANDANI22@users.noreply.github.com>
2026-02-08 09:11:36 +0530

    Time: 382 ms (15.96%), Space: 43 MB (28.74%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(int i , int j, char k , char[][] board){
+        for(int var = 0; var <9; var++){
+            if(board[i][var] == k || board[var][j] == k) return false;
+            if(board[3 *(i/3)+var /3] [3*(j/3)+var%3] == k) return false;
+        }
+        return true;
+    }
+    public boolean solve(char[][] board){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] == '.'){
+                    for(char k='1'; k <= '9'; k++){
+                        if(isValid(i,j,k,board)){
+                            board[i][j] = k;
+                            if(solve(board)) return true;
+                            board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+}
