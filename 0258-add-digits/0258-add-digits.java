ff19064fc2ba1da42de08ea602023f1ce9abb3b3
Nandani Masurkar <136251117+NANDANI22@users.noreply.github.com>
2026-03-02 19:34:02 +0530

    Time: 1 ms (97.86%), Space: 42.5 MB (81.86%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num==0) return 0;
+        while(num>=10){
+        int sum = 0;
+        while(num>0){
+            int lastDigit = num % 10;
+            sum = sum + lastDigit;
+            num/=10;
+        }
+        num = sum;
+        }
+        return num;
+    }
+}
